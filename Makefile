9d1142922cd3b70f50e6cb6ba857be392b66b453
Bogdan1726 <bogdan24ro@gmail.com>
2022-08-03 12:13:32 +0300

    add apps "ads, housing, users"

---
+++ Makefile
@@ -0,0 +1,32 @@
+MANAGE = python manage.py
+PROJECT = swipe
+
+# Local
+run:
+	$(MANAGE) runserver
+
+migrations:
+	$(MANAGE) makemigrations
+
+migrate:
+	$(MANAGE) migrate
+
+superuser:
+	$(MANAGE) createsuperuser
+
+# Docker
+
+#start_worker:
+#	celery -A myhause24 worker -l info
+#
+#start_app:
+#	$(MANAGE) migrate --no-input
+#	$(MANAGE) loaddata dump.json
+#	$(MANAGE) collectstatic --no-input
+#	gunicorn myhause24.wsgi:application --bind 0.0.0.0:8000
+
+#shell:
+#	docker exec -it container_cinema python manage.py shell
+#
+#dump_data:
+#	docker exec -it container_cinema python manage.py loaddata db.json
--- swipe/settings.py
+++ swipe/settings.py
@@ -11,21 +11,26 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_&*n!(lf5wu)6160#6n3@@s0ps&_@t+*qk6nc-t4ziwg6h=p_h'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    'ads.apps.AdsConfig',
+    'housing.apps.HousingConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +83,12 @@ WSGI_APPLICATION = 'swipe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT')
     }
 }
 
@@ -103,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
