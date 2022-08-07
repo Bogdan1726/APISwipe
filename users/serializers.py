2db68016d6b203cb9389a6f4e90bb7db8fe9e85a
Bogdan1726 <bogdan24ro@gmail.com>
2022-08-07 18:21:15 +0300

    add login and registration

---
+++ users/serializers.py
@@ -0,0 +1,38 @@
+from dj_rest_auth.registration.serializers import RegisterSerializer
+from dj_rest_auth.serializers import LoginSerializer
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class CustomLoginSerializer(LoginSerializer):
+
+    username = None
+
+
+class CustomRegisterSerializer(RegisterSerializer):
+    username = None
+    first_name = serializers.CharField(
+        max_length=150,
+        min_length=2,
+        required=True,
+    )
+    last_name = serializers.CharField(
+        max_length=150,
+        min_length=2,
+        required=True,
+    )
+
+    def get_cleaned_data(self):
+        return {
+            'email': self.validated_data.get('email', ''),
+            'password1': self.validated_data.get('password1', ''),
+            'password2': self.validated_data.get('password2', ''),
+            'first_name': self.validated_data.get('first_name', ''),
+            'last_name': self.validated_data.get('last_name', ''),
+        }
+
+    def save(self, request):
+        user = super().save(request)
+        return user
